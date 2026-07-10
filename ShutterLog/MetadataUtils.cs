91384081ebe711c61b86ef78d864bcb67fcb4342
changsun20 <110759360+changsun20@users.noreply.github.com>
2026-07-10 14:59:47 +0800

    chore: use camelCase for local var

--- ShutterLog/MetadataUtils.cs
+++ ShutterLog/MetadataUtils.cs
@@ -8,10 +8,10 @@ class MetadataUtils
     {
         var directories = ImageMetadataReader.ReadMetadata(file);
 
-        int ISO = 0;
-        int ExposureTimeDenominator = 0;
-        double FNumber = 0.0;
-        double FocalLength = 0.0;
+        int iso = 0;
+        int exposureTimeDenominator = 0;
+        double fNumber = 0.0;
+        double focalLength = 0.0;
         foreach (var directory in directories)
         {
             if (directory.Name == "Exif SubIFD")
@@ -21,56 +21,56 @@ class MetadataUtils
                     var description = tag.Description;
                     if (tag.Name == "Focal Length")
                     {
-                        FocalLength = ParseFocalLength(description);
+                        focalLength = ParseFocalLength(description);
                     }
                     if (tag.Name == "ISO Speed Ratings")
                     {
-                        ISO = ParseISO(description);
+                        iso = ParseISO(description);
                     }
                     if (tag.Name == "Exposure Time")
                     {
-                        ExposureTimeDenominator = ParseExposureTimeDenominator(description);
+                        exposureTimeDenominator = ParseExposureTimeDenominator(description);
                     }
                     if (tag.Name == "F-Number")
                     {
-                        FNumber = ParseFNumber(description);
+                        fNumber = ParseFNumber(description);
                     }
                 }
             }
         }
-        return new Image(FocalLength, ExposureTimeDenominator, FNumber, ISO);
+        return new Image(focalLength, exposureTimeDenominator, fNumber, iso);
     }
 
-    private static int ParseISO(string TagDescription)
+    private static int ParseISO(string tagDescription)
     {
-        return int.Parse(TagDescription);
+        return int.Parse(tagDescription);
     }
 
-    private static int ParseExposureTimeDenominator(string TagDescription)
+    private static int ParseExposureTimeDenominator(string tagDescription)
     {
         // e.g. 1/60 s
-        if (TagDescription.Contains('/'))
+        if (tagDescription.Contains('/'))
         {
-            return int.Parse(TagDescription.Split("/")[1].Split(" ")[0]);
+            return int.Parse(tagDescription.Split("/")[1].Split(" ")[0]);
         }
         // e.g. 0.02 s
         else
         {
-            double time = double.Parse(TagDescription.Split(" ")[0]);
+            double time = double.Parse(tagDescription.Split(" ")[0]);
             int denominator = int.Parse($"{1 / time}".Split(".")[0]);
             return denominator;
         }
     }
 
-    private static double ParseFNumber(string TagDescription)
+    private static double ParseFNumber(string tagDescription)
     {
         // e.g. f/1.8
-        return double.Parse(TagDescription.Split("/")[1]);
+        return double.Parse(tagDescription.Split("/")[1]);
     }
 
-    private static double ParseFocalLength(string TagDescription)
+    private static double ParseFocalLength(string tagDescription)
     {
         // e.g. 50 mm
-        return double.Parse(TagDescription.Split(" ")[0]);
+        return double.Parse(tagDescription.Split(" ")[0]);
     }
 }
