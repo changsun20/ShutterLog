f607eed3fb5745c19c64257d2984964174189fc2
changsun20 <110759360+changsun20@users.noreply.github.com>
2026-07-10 23:19:17 +0800

    chore: beautify ui

--- ShutterLog/Visualize.cs
+++ ShutterLog/Visualize.cs
@@ -6,24 +6,38 @@ class Visualize
 {
     public static void VisualizeAll(List<Image> images)
     {
-        var focalLengthPanel = buildPanel(Stat.GetFocalLengthStat(images), "Focal Length");
-        var exposureTimePanel = buildPanel(Stat.GetExposureTimeStat(images), "Exposure Time");
-        var fNumberPanel = buildPanel(Stat.GetFNumberStat(images), "F-Number");
-        var isoPanel = buildPanel(Stat.GetISOStat(images), "ISO");
+        AnsiConsole.Write(new Rule("ShutterLog Report").RuleStyle("bold").Centered());
+
+        var focalLengthPanel = buildPanel(
+            Stat.GetFocalLengthStat(images),
+            "Focal Length",
+            Color.DodgerBlue1
+        );
+        var exposureTimePanel = buildPanel(
+            Stat.GetExposureTimeStat(images),
+            "Exposure Time",
+            Color.SteelBlue
+        );
+        var fNumberPanel = buildPanel(Stat.GetFNumberStat(images), "F-Number", Color.CadetBlue);
+        var isoPanel = buildPanel(Stat.GetISOStat(images), "ISO", Color.DarkCyan);
 
         var grid = buildGrid(focalLengthPanel, exposureTimePanel, fNumberPanel, isoPanel);
 
         AnsiConsole.Write(grid);
+        AnsiConsole.MarkupLine($"  [grey]Total photos:[/] [bold]{images.Count}[/]");
     }
 
-    private static Panel buildPanel(List<(string, int)> hist, string header)
+    private static Panel buildPanel(List<(string, int)> hist, string header, Color color)
     {
         var chart = new BarChart();
         foreach (var (label, count) in hist)
         {
-            chart.AddItem(label, count);
+            chart.AddItem(label, count, color);
         }
-        var panel = new Panel(chart).Header(header, Justify.Center).RoundedBorder();
+        var panel = new Panel(chart)
+            .Header(header, Justify.Center)
+            .RoundedBorder()
+            .BorderColor(color);
         return panel;
     }
 
