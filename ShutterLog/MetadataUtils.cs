4a0bdb80a4116c9ebdaed1110647d9915d1fff09
changsun20 <110759360+changsun20@users.noreply.github.com>
2026-07-10 14:44:35 +0800

    fix: WIP better EXIF data parsing

--- ShutterLog/MetadataUtils.cs
+++ ShutterLog/MetadataUtils.cs
@@ -22,22 +22,18 @@ class MetadataUtils
                     if (tag.Name == "Focal Length")
                     {
                         FocalLength = ParseFocalLength(description);
-                        Console.WriteLine(FocalLength);
                     }
                     if (tag.Name == "ISO Speed Ratings")
                     {
                         ISO = ParseISO(description);
-                        Console.WriteLine(ISO);
                     }
                     if (tag.Name == "Exposure Time")
                     {
                         ExposureTimeDenominator = ParseExposureTimeDenominator(description);
-                        Console.WriteLine(ExposureTimeDenominator);
                     }
                     if (tag.Name == "F-Number")
                     {
                         FNumber = ParseFNumber(description);
-                        Console.WriteLine(FNumber);
                     }
                 }
             }
@@ -52,16 +48,29 @@ class MetadataUtils
 
     private static int ParseExposureTimeDenominator(string TagDescription)
     {
-        return int.Parse(TagDescription.Split("/")[1].Split(" ")[0]);
+        // e.g. 1/60 s
+        if (TagDescription.Contains('/'))
+        {
+            return int.Parse(TagDescription.Split("/")[1].Split(" ")[0]);
+        }
+        // e.g. 0.02 s
+        else
+        {
+            double time = double.Parse(TagDescription.Split(" ")[0]);
+            int denominator = int.Parse($"{1 / time}".Split(".")[0]);
+            return denominator;
+        }
     }
 
     private static double ParseFNumber(string TagDescription)
     {
+        // e.g. f/1.8
         return double.Parse(TagDescription.Split("/")[1]);
     }
 
     private static double ParseFocalLength(string TagDescription)
     {
+        // e.g. 50 mm
         return double.Parse(TagDescription.Split(" ")[0]);
     }
 }
